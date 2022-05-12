a1ab161c2d21e8e869c5041047ae71a89826241f
XYphrodite <72796194+XYphrodite@users.noreply.github.com>
2022-05-12 23:51:41 +0300

    Добавьте файлы проекта.

---
+++ Labs_OS4/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Labs_OS4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество потоков:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(546, 211);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "#";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Имя процесса";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ID";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Потоков";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Память";
+            this.Column5.Name = "Column5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Все потоки:";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.dataGridView2.Location = new System.Drawing.Point(16, 257);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(546, 211);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "#";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID потока";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "ID процесса";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "базовый класс приоритета";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "дельта-приоритет";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(739, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Модули";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Узнать модули";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(640, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Информация о текущем процессе:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(640, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(640, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(640, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(640, 429);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(173, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Информация о текущем модуле:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(640, 442);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(640, 455);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "label10";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(568, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(406, 225);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 480);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Labs_OS4/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Labs_OS4
+{
+    public partial class Form1 : Form
+    {
+        private struct PROCESS_MEMORY_COUNTERS
+        {
+            public uint cb;
+            public uint PageFaultCount;
+            public UInt64 PeakWorkingSetSize;
+            public UInt64 WorkingSetSize;
+            public UInt64 QuotaPeakPagedPoolUsage;
+            public UInt64 QuotaPagedPoolUsage;
+            public UInt64 QuotaPeakNonPagedPoolUsage;
+            public UInt64 QuotaNonPagedPoolUsage;
+            public UInt64 PagefileUsage;
+            public UInt64 PeakPagefileUsage;
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
+        public enum SnapshotFlags : uint
+        {
+            HeapList = 0x00000001,
+            Process = 0x00000002,
+            Thread = 0x00000004,
+            Module = 0x00000008,
+            Module32 = 0x00000010,
+            All = (HeapList | Process | Thread | Module),
+            Inherit = 0x80000000,
+            NoHeaps = 0x40000000
+
+        }
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)] public string szExeFile;
+        };
+        public struct THREADENTRY32
+        {
+
+            internal UInt32 dwSize;
+            internal UInt32 cntUsage;
+            internal UInt32 th32ThreadID;
+            internal UInt32 th32OwnerProcessID;
+            internal UInt32 tpBasePri;
+            internal UInt32 tpDeltaPri;
+            internal UInt32 dwFlags;
+        }
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+        [DllImport("kernel32.dll")]
+        static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+        [DllImport("kernel32.dll")]
+        static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, uint processId);
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess((uint)flags, false, (uint)proc.Id);
+        }
+        [DllImport("psapi.dll", SetLastError = true)]
+        static extern bool GetProcessMemoryInfo(IntPtr hProcess, out PROCESS_MEMORY_COUNTERS counters, uint size);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr hHandle);
+        [DllImport("kernel32.dll")]
+        static extern bool Thread32First(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+        [DllImport("kernel32.dll")]
+        static extern bool Thread32Next(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+        [DllImport("kernel32.dll")]
+        static extern uint GetCurrentProcessId();
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+        enum ThreadPriority
+        {
+            THREAD_MODE_BACKGROUND_BEGIN = 0x00010000,
+            THREAD_MODE_BACKGROUND_END = 0x00020000,
+            THREAD_PRIORITY_ABOVE_NORMAL = 1,
+            THREAD_PRIORITY_BELOW_NORMAL = -1,
+            THREAD_PRIORITY_HIGHEST = 2,
+            THREAD_PRIORITY_IDLE = -15,
+            THREAD_PRIORITY_LOWEST = -2,
+            THREAD_PRIORITY_NORMAL = 0,
+            THREAD_PRIORITY_TIME_CRITICAL = 15
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool DuplicateHandle(IntPtr hSourceProcessHandle,
+        IntPtr hSourceHandle, IntPtr hTargetProcessHandle, out IntPtr lpTargetHandle,
+        uint dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, uint dwOptions);
+        [Flags]
+        public enum DuplicateOptions : uint
+        {
+            DUPLICATE_CLOSE_SOURCE = (0x00000001),// Closes the source handle. This occurs regardless of any error status returned.
+            DUPLICATE_SAME_ACCESS = (0x00000002), //Ignores the dwDesiredAccess parameter. The duplicate handle has the same access as the source handle.
+        }
+        [DllImport("psapi.dll")]
+        static extern uint GetModuleFileNameEx(int hProcess, int hModule, [Out] StringBuilder lpBaseName, [In][MarshalAs(UnmanagedType.U4)] int nSize);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] in string lpModuleName);
+        [StructLayout(LayoutKind.Sequential, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public struct MODULEENTRY32
+        {
+            internal uint dwSize;
+            internal uint th32ModuleID;
+            internal uint th32ProcessID;
+            internal uint GlblcntUsage;
+            internal uint ProccntUsage;
+            internal IntPtr modBaseAddr;
+            internal uint modBaseSize;
+            internal IntPtr hModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            internal string szModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            internal string szExePath;
+        }
+        [DllImport("kernel32.dll")]
+        static extern bool Module32First(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+        [DllImport("kernel32.dll")]
+        static extern bool Module32Next(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+
+        public Form1()
+        {
+            InitializeComponent();
+            int amount_of_threads = 0;
+            int amount_of_process = 0;
+            IntPtr h;
+            IntPtr h2;
+            PROCESS_MEMORY_COUNTERS pmc;
+            int cb;
+            IntPtr hSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.Process, 0);
+            PROCESSENTRY32 lppe = new PROCESSENTRY32();
+            THREADENTRY32 lpte = new THREADENTRY32();
+            StringBuilder modName = new StringBuilder("ABC",255);
+            if (hSnapshot.Equals(0))
+            {
+                Close();
+            }
+            lppe.dwSize = (uint)Marshal.SizeOf(typeof(PROCESSENTRY32));
+            if (Process32First(hSnapshot, ref lppe))
+            {
+                while (Process32Next(hSnapshot, ref lppe))
+                {
+                    amount_of_process++;
+                    dataGridView1.Rows.Add(amount_of_process.ToString(), Path.GetFileName(lppe.szExeFile), lppe.th32ProcessID.ToString(), ((int)lppe.cntThreads).ToString(), "Unknown");
+                    amount_of_threads += (int)lppe.cntThreads;
+                    h = OpenProcess(0x00000400, false, lppe.th32ProcessID);
+                    if ((int)h != 0)
+                    {
+                        cb = (int)Marshal.SizeOf(typeof(PROCESS_MEMORY_COUNTERS));
+                        pmc = new PROCESS_MEMORY_COUNTERS();
+                        pmc.cb = (uint)cb;
+                        if (GetProcessMemoryInfo(h, out pmc, (uint)cb))
+                        {
+                            dataGridView1.Rows[(int)amount_of_process - 1].Cells[4].Value = (pmc.WorkingSetSize / 1024).ToString() + " kb";
+                        }
+                        else
+                        {
+                            dataGridView1.Rows[(int)amount_of_process - 1].Cells[4].Value = "Unknown";
+                            //FreeMen(pmc);
+                            CloseHandle(h);
+                        }
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[(int)amount_of_process - 1].Cells[4].Value = "Unknown";
+                    }
+                }
+                label1.Text = "Всего потоков: " + amount_of_threads.ToString();
+
+            }
+            lpte.dwSize = (uint)Marshal.SizeOf(typeof(THREADENTRY32));
+            hSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.Thread, 0);
+            if (hSnapshot.Equals(0))
+            {
+                Close();
+            }
+            amount_of_threads = 0;
+            if (Thread32First(hSnapshot, ref lpte))
+            {
+                while (Thread32Next(hSnapshot, ref lpte))
+                {
+                    amount_of_threads++;
+                    dataGridView2.Rows.Add(amount_of_threads.ToString(), lpte.th32ThreadID.ToString(), lpte.th32OwnerProcessID.ToString(), "Uknown", "Unknown");
+                    switch ((int)lpte.tpBasePri)
+                    {
+                        case 4:
+                            dataGridView2[3, amount_of_threads].Value = "ожидающий";
+                            break;
+                        case 8:
+                            dataGridView2[3, amount_of_threads].Value = "нормальный";
+                            break;
+                        case 13:
+                            dataGridView2[3, amount_of_threads].Value = "высокий";
+                            break;
+                        case 24:
+                            dataGridView2[3, amount_of_threads].Value = "реального времени";
+                            break;
+                        default:
+                            dataGridView2[3, amount_of_threads].Value = "Unknown";
+                            break;
+                    }
+                    switch ((int)lpte.tpDeltaPri)
+                    {
+                        case -15:
+                            dataGridView2[4, amount_of_threads].Value = "Idle";
+                            break;
+                        case -2:
+                            dataGridView2[4, amount_of_threads].Value = "the lowest";
+                            break;
+                        case -1:
+                            dataGridView2[4, amount_of_threads].Value = "delow normal";
+                            break;
+                        case -0:
+                            dataGridView2[4, amount_of_threads].Value = "normal";
+                            break;
+                        case 1:
+                            dataGridView2[4, amount_of_threads].Value = "above normal";
+                            break;
+                        case 2:
+                            dataGridView2[4, amount_of_threads].Value = "the highest";
+                            break;
+                        case 15:
+                            dataGridView2[4, amount_of_threads].Value = "time critical";
+                            break;
+                        default:
+                            dataGridView2[4, amount_of_threads].Value = "Unknown";
+                            break;
+
+                    }
+                }
+            }
+            label5.Text = "ID процесса: " + GetCurrentProcessId();
+            label6.Text = "Псевдодескриптор: " + GetCurrentProcess();
+            DuplicateHandle((IntPtr)GetCurrentProcess(), (IntPtr)GetCurrentProcess(), (IntPtr)GetCurrentProcess(), out h, 0, false, (uint)DuplicateOptions.DUPLICATE_SAME_ACCESS);
+            h2 = OpenProcess((uint)ProcessAccessFlags.All, false, GetCurrentProcessId());
+            label7.Text = "Копия дескриптора текущего процесса: " + h2.ToString();
+            CloseHandle(h);
+            CloseHandle(h2);
+            GetModuleFileNameEx(0, 0, modName, 255);
+
+            label9.Text = modName.ToString();
+            label10.Text = "Дескриптор модуля: " + GetModuleHandle(label9.Text).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            MODULEENTRY32 lpme = new MODULEENTRY32();
+            lpme.dwSize = (uint)Marshal.SizeOf(lpme);
+            int id = int.Parse(dataGridView1.CurrentRow.Cells[2].Value.ToString());
+            IntPtr hSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.Module, (uint)id);
+            Module32First(hSnapshot, ref lpme);
+            Encoding CN = Encoding.Unicode;
+            Encoding unicode = Encoding.ASCII;
+            do
+            {
+                byte[] unicodeBytes = CN.GetBytes(lpme.szModule);
+                string CNString = unicode.GetString(unicodeBytes);
+                richTextBox1.Text += CNString + Environment.NewLine;
+
+            } while (Module32Next(hSnapshot, ref lpme));
+            CloseHandle(hSnapshot);
+        }
+    }
+}
+
